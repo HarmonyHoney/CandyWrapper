846db675e62c725cf134719e65218a1fd98b9351
Harmony Honey Monroe <harmonymonroe@proton.me>
2025-07-17 22:44:10 -0400

    coyote frames for jumping ! 6 frames

--- src/game/player.gd
+++ src/game/player.gd
@@ -14,8 +14,11 @@ var term_vel := 400.0
 
 var is_floor := false
 var is_jump := false
+var air_frames := 0
+var coyote_frames := 6
 
 func _physics_process(delta):
+	
 	# gravity
 	vel.y += grv * delta
 	vel.y = clamp(vel.y, -term_vel, term_vel)
@@ -25,12 +28,13 @@ func _physics_process(delta):
 	vel.x = btnx * spd
 	
 	# jump
-	if is_floor:
+	if air_frames <= coyote_frames:
 		if game.btnp("jump"):
 			is_jump = true
 			vel.y = -jump_vel
 			audio.pitch_scale = randf_range(0.9, 1.1)
 			audio.play()
+			air_frames = 100
 	elif is_jump:
 		if !game.btnd("jump") and vel.y < jump_vel / -3:
 			is_jump = false
@@ -47,6 +51,9 @@ func _physics_process(delta):
 			vel.y = 0
 		# check for floor 0.1 pixel down
 		is_floor = test_move(transform, Vector2(0, 0.1))
+	
+	# coyote frames
+	air_frames = 0 if is_floor else air_frames + 1
 	
 	# sprite flip
 	if btnx != 0:
